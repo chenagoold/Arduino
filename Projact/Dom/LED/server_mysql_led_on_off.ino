dd8c4024ca2a0c1f413e648880b40eb70eda366b
chenagoold <61205251+chenagoold@users.noreply.github.com>
2020-11-24 15:39:52 +0300

    Update server_mysql_led_on_off.ino

--- Projact/Dom/LED/server_mysql_led_on_off.ino
+++ Projact/Dom/LED/server_mysql_led_on_off.ino
@@ -43,72 +43,72 @@ void setup() {
   //----------------------------------------
   digitalWrite(ON_Board_LED, HIGH); //--> Установлино соединение установлено
   //----------------------------------------Для Успешное подключение выводим следующие данные  
-  Serial.println("");
+  Serial.println("");//Вывод
   Serial.print("Successfully connected to : ");
   Serial.println(ssid);
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());//Возращает локайльный адресс 
   Serial.println();
   //----------------------------------------
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  HTTPClient http; //--> Declare object of class HTTPClient
+  HTTPClient http; //объект класса HTTPClient 
 
-  //----------------------------------------Getting Data from MySQL Database
-  String GetAddress, LinkGet, getData;
-  String LEDStatResultSend;
-  int id = 0; //--> ID in Database
-  GetAddress = "led/GetData.php"; 
-  LinkGet = host + GetAddress; //--> Make a Specify request destination
+  //---------------------------------------- from MySQL Database
+  String GetAddress, LinkGet, getData; //строковые переменные 
+  String LEDStatResultSend;//строковые переменные 
+  int id = 0; // 
+  GetAddress = "led/GetData.php"; //Адресс запроса  
+  LinkGet = host + GetAddress; //http://192.168.1.238/ + led/GetData.php
   getData = "id=" + String(id);
   Serial.println("----------------Connect to Server-----------------");
   Serial.println("Get LED Status from Server or Database");
   Serial.print("Request Link : ");
-  Serial.println(LinkGet);
-  http.begin(LinkGet); //--> Specify request destination
+  Serial.println(LinkGet);//Вывод  http://192.168.1.238/led/GetData.php
+  http.begin(LinkGet); //Выполняем http запрос http://192.168.1.238/led/GetData.php
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");    //Specify content-type header
-  int httpCodeGet = http.POST(getData); //--> Send the request
-  String payloadGet = http.getString(); //--> Get the response payload from server
+  int httpCodeGet = http.POST(getData); //--> Запрос на сервер есть в базе один или ноль 
+  String payloadGet = http.getString(); //--> Ответ на запрос и записываем в переменную payloadGet=1
   Serial.print("Response Code : "); //--> If Response Code = 200 means Successful connection, if -1 means connection failed. For more information see here : https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-  Serial.println(httpCodeGet); //--> Print HTTP return code
+  Serial.println(httpCodeGet); //--> Print HTTP return Возращает вывод HTTP 
   Serial.print("Returned data from Server : ");
-  Serial.println(payloadGet); //--> Print request response payload
+  Serial.println(payloadGet); //--> Print request response payload Ответ на запрос  выводим 
 
-  if (payloadGet == "1") {
+  if (payloadGet == "1") { // Проверяем ответ если еденица зажигаем 
     digitalWrite(D8, HIGH); //--> Turn off Led
     LEDStatResultSend = payloadGet;
   }
-  if (payloadGet == "0") {
+  if (payloadGet == "0") { // Ноль отключаем
     digitalWrite(D8, LOW); //--> Turn off Led
     LEDStatResultSend = payloadGet;
   }
 
-  //----------------------------------------Sends LED status feedback data to server
-Serial.println();
-Serial.println("Sending LED Status to Server");
+  //----------------------------------------Запускает скрипт вывод статуса 
+Serial.println();//вывод
+Serial.println("Sending LED Status to Server");//вывод
 String postData, LinkSend, SendAddress;
-SendAddress = "led/getLEDStatFromNodeMCU.php";
-LinkSend = host + SendAddress;
-postData = "getLEDStatusFromNodeMCU=" + LEDStatResultSend;
+SendAddress = "led/getLEDStatFromNodeMCU.php";// присвоим строковое значение 
+LinkSend = host + SendAddress;// http://192.168.1.238/ + led/getLEDStatFromNodeMCU.php
+postData = "getLEDStatusFromNodeMCU=" + LEDStatResultSend; //getLEDStatusFromNodeMCU=1 
 Serial.print("Request Link : ");
-Serial.println(LinkSend);
-http.begin(LinkSend); //--> Specify request destination
-http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //--> Specify content-type header
-int httpCodeSend = http.POST(postData); //--> Send the request
-String payloadSend = http.getString(); //--> Get the response payload
+Serial.println(LinkSend);//http://192.168.1.238/ + led/getLEDStatFromNodeMCU.php
+http.begin(LinkSend); //--> Запуск http://192.168.1.238/ + led/getLEDStatFromNodeMCU.php
+http.addHeader("Content-Type", "application/x-www-form-urlencoded"); //--> 
+int httpCodeSend = http.POST(postData); //--> getLEDStatusFromNodeMCU=1
+String payloadSend = http.getString(); //--> Get the response payload получение полезной нагрузки 
 Serial.print("Response Code : "); //--> If Response Code = 200 means Successful connection, if -1 means connection failed
-Serial.println(httpCodeSend); //--> Print HTTP return code
-Serial.print("Returned data from Server : ");
-Serial.println(payloadSend); //--> Print request response payload
+Serial.println(httpCodeSend); //--> Print HTTP return code вывод запроса 
+Serial.print("Returned data from Server : "); // 
+Serial.println(payloadSend); //--> Print request response payload Вывести ответ на запрос
 //----------------------------------------
   //----------------------------------------
   
-  Serial.println("----------------Closing Connection----------------");
-  http.end(); //--> Close connection
+  Serial.println("----------------Closing Connection----------------");//Вывод 
+  http.end(); //--> Close connection закрыть подключение http
   Serial.println();
   Serial.println("Please wait 5 seconds for the next connection.");
   Serial.println();
-  delay(5000); //--> GET Data at every 5 seconds
+  delay(5000); //--> GET Data at every 5 seconds следующие подключение 
 }
